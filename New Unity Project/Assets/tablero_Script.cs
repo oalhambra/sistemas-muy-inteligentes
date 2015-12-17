7a84c4e2518ed627bd840103f00c800d8c150730
oalhambra <o.alhambra@gmail.com>
2015-12-17 19:22:50 +0100

    trabajo 14-12

--- New Unity Project/Assets/tablero_Script.cs
+++ New Unity Project/Assets/tablero_Script.cs
@@ -1,29 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class tablero_Script : MonoBehaviour {
+public class tablero_Script : MonoBehaviour
+{
 
-	// Use this for initialization
+    // Use this for initialization
     public const int AZUL = 1;
     public const int ROJO = 2;
     public const int VERDE = 3;
     public const int MORADO = 4;
     public const int PUNTOS = 20;
     //tablero =0 -> vacio 
-    int[,] tablero=new int[25,25];
-	void Start () {
+    int[,] tablero = new int[25, 25];
+    void Start()
+    {
         //inicializacion de las variables
         //cada ciudad se corresponde con un numero
-
+        Random.seed = (int)System.DateTime.Now.Millisecond;
         inicializarTablero();
-	    
-        
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 
     private void inicializarTablero()
     {
@@ -40,37 +43,32 @@ public class tablero_Script : MonoBehaviour {
         tablero[0, tablero.Length] = VERDE;
         tablero[tablero.Length, tablero.Length] = MORADO;
     }
-    private Ciudad reproducir(Ciudad c1, Ciudad c2){
-        Ciudad res=null;
-        
-        return res;
 
-    }
-    private class Ciudad{
+    private class Ciudad
+    {
         int color;
         int[] stats = new int[PUNTOS];
         const int CIUDADANOS = 1;
         const int MILITAR = 2;
         const int MINAS = 3;
         const int GRANJAS = 4;
+        //const int VALENTIA = 5;
+
         int poblacion;
         int ejercito;
         int fitness;
-        
-
 
         public Ciudad()
         {
-            Random.seed=(int)System.DateTime.Now.Millisecond;
-            System.Threading.Thread.Sleep(1);
+            
             inicializarStats();
-            int randomNumber = Random.Range(1, 5);
+            //int randomNumber = Random.Range(1, 5);
 
         }
-        public Ciudad(int[] stats){
+        public Ciudad(int[] stats)
+        {
             //si te explota en la cara arreglalo
             this.stats = stats;
-
         }
         private void inicializarStats()
         {
@@ -79,10 +77,20 @@ public class tablero_Script : MonoBehaviour {
                 stats[i] = Random.Range(1, 5);
             }
         }
+        private static Ciudad reproducir(Ciudad ciudad1, Ciudad ciudad2)
+        {
+            int[] stats = new int[PUNTOS];
+            int randomNumber = Random.Range(0, PUNTOS + 1);
+            for (int i = 0; i < randomNumber; i++)
+            {
+                stats[i] = ciudad1.stats[i];
+            }
+            for (int i = randomNumber; i < stats.Length; i++)
+            {
+                stats[i] = ciudad2.stats[i];
+            }
 
-       
-
-
+            return new Ciudad(stats);
+        }
     }
-
 }
