a126237eaba9c2427019e895b576f67e2e9744e5
Manuel Casariego <manucasariego@gmail.com>
2016-01-18 22:45:30 +0100

    Update tablero_Script.cs

--- New Unity Project/Assets/tablero_Script.cs
+++ New Unity Project/Assets/tablero_Script.cs
@@ -1,8 +1,7 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
-public class tablero_Script : MonoBehaviour
-{
+public class tablero_Script : MonoBehaviour {
 
     // Use this for initialization
     public const int AZUL = 1;//#0000FF
@@ -26,17 +25,13 @@ public class tablero_Script : MonoBehaviour
     private GameObject ciudad;
     private int estado;
 
-    void Start()
-    {
+    void Start() {
         //inicializacion de las variables
         //cada ciudad se corresponde con un numero
-        for (int i = 0; i < arr_ciudades.Length; i++)
-        {
+        for (int i = 0; i < arr_ciudades.Length; i++) {
             arr_ciudades[i] = new Ciudad();
         }
         combate = new Combate(arr_ciudades[0], arr_ciudades[0], arr_ciudades[0], arr_ciudades[0]);
-        combate.inicializarTablero();
-        //Debug.Log(tablero.Length);
         graficosIni();
         Random.seed = (int)System.DateTime.Now.Millisecond;
         actualizarGraficos();
@@ -44,20 +39,17 @@ public class tablero_Script : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
 
         //estado 1, combatiendo
-        if (estado == 1)
-        {
+        if (estado == 1) {
             Debug.Log("1");
             combate.ejecutarCombate();
             Debug.Log("hola holita");
             //estado = 3;
         }
         //estado 2, transicion entre combates
-        if (estado == 2)
-        {
+        if (estado == 2) {
             //despejar el tablero
             Debug.Log("estado 2");
             estado = 1;
@@ -65,15 +57,13 @@ public class tablero_Script : MonoBehaviour
 
         }
         //estado 3, reproduccion
-        if (estado == 3)
-        {
+        if (estado == 3) {
             //seleccionar pareja a reproducir en funcion de fitness
 
             //ejecutar reproduccion
 
         }
-        if (estado == 0)
-        {
+        if (estado == 0) {
             Debug.Log("estado 0");
             estado = 2;
         }
@@ -96,12 +86,9 @@ public class tablero_Script : MonoBehaviour
 
 
 
-    private void graficosIni()
-    {
-        for (int i = 0; i < TAMAÑO; i++)
-        {
-            for (int j = 0; j < TAMAÑO; j++)
-            {
+    private void graficosIni() {
+        for (int i = 0; i < TAMAÑO; i++) {
+            for (int j = 0; j < TAMAÑO; j++) {
                 tableroGrafico[i, j] = (GameObject)Instantiate(ciudad, new Vector3(5 * i, 0, 5 * j), new Quaternion());
             }
         }
@@ -115,24 +102,19 @@ public class tablero_Script : MonoBehaviour
         Debug.Log("test2 con color"); Debug.Log(tablero.Length);
          */
     }
-    private void actualizarGraficos()
-    {
-        for (int i = 0; i < TAMAÑO; i++)
-        {
-            for (int j = 0; j < TAMAÑO; j++)
-            {
+    private void actualizarGraficos() {
+        for (int i = 0; i < TAMAÑO; i++) {
+            for (int j = 0; j < TAMAÑO; j++) {
                 pintarCuadro(i, j);
             }
         }
     }
 
-    private void pintarCuadro(int i, int j)
-    {
+    private void pintarCuadro(int i, int j) {
         int[,] tablero = combate.getTablero();
         int aux = tablero[i, j];
         Color color = new Color(1, 1, 1, 1);
-        switch (aux)
-        {
+        switch (aux) {
             case 1:
                 color = colorAzul;
                 break;
@@ -163,8 +145,7 @@ public class tablero_Script : MonoBehaviour
     //////////////////////////////////////////////////////////////////////////////
 
 
-    public class Ciudad
-    {
+    public class Ciudad {
         int color;
         int[] stats = new int[PUNTOS];
         const int CIUDADANOS = 1;
@@ -179,62 +160,49 @@ public class tablero_Script : MonoBehaviour
 
         int tamaño;
 
-        public Ciudad()
-        {
+        public Ciudad() {
 
             inicializarStats();
             //int randomNumber = Random.Range(1, 5);
 
         }
-        public Ciudad(int[] stats)
-        {
+        public Ciudad(int[] stats) {
             //si te explota en la cara arreglalo
             this.stats = stats;
         }
-        private void inicializarStats()
-        {
-            for (int i = 0; i < stats.Length; i++)
-            {
+        private void inicializarStats() {
+            for (int i = 0; i < stats.Length; i++) {
                 stats[i] = Random.Range(1, 5);
             }
         }
-        private static Ciudad reproducir(Ciudad ciudad1, Ciudad ciudad2)
-        {
+        private static Ciudad reproducir(Ciudad ciudad1, Ciudad ciudad2) {
             int[] stats = new int[PUNTOS];
             int randomNumber = Random.Range(0, PUNTOS + 1);
-            for (int i = 0; i < randomNumber; i++)
-            {
+            for (int i = 0; i < randomNumber; i++) {
                 stats[i] = ciudad1.stats[i];
             }
-            for (int i = randomNumber; i < stats.Length; i++)
-            {
+            for (int i = randomNumber; i < stats.Length; i++) {
                 stats[i] = ciudad2.stats[i];
             }
 
             return new Ciudad(stats);
         }
-        public int getPoblacion()
-        {
+        public int getPoblacion() {
             return poblacion;
         }
-        public int getEjercito()
-        {
+        public int getEjercito() {
             return ejercito;
         }
-        public int getTamaño()
-        {
+        public int getTamaño() {
             return tamaño;
         }
-        public void setPoblacion(int poblacion)
-        {
+        public void setPoblacion(int poblacion) {
             this.poblacion = poblacion;
         }
-        public void setEjercito(int ejercito)
-        {
+        public void setEjercito(int ejercito) {
             this.ejercito = ejercito;
         }
-        public void setTamaño(int tamaño)
-        {
+        public void setTamaño(int tamaño) {
             this.tamaño = tamaño;
         }
 
@@ -251,20 +219,15 @@ public class tablero_Script : MonoBehaviour
             int[,] movimientosPosibles = new int[TAMAÑO, TAMAÑO];
 
             // Crear funcion ponerCosoACeros si eso
-            for (int i = 0; i < TAMAÑO; i++)
-            {
-                for (int j = 0; j < TAMAÑO; j++)
-                {
+            for (int i = 0; i < TAMAÑO; i++) {
+                for (int j = 0; j < TAMAÑO; j++) {
                     movimientosPosibles[i, j] = 0;
                 }
             }
-            for (int i = 0; i < TAMAÑO; i++)
-            {
-                for (int j = 0; j < TAMAÑO; j++)
-                {
+            for (int i = 0; i < TAMAÑO; i++) {
+                for (int j = 0; j < TAMAÑO; j++) {
                     // TODO: Este color hay que inicializarlo
-                    if (tablero[i, j] == this.color)
-                    {
+                    if (tablero[i, j] == this.color) {
                         // Sacar esto a una funcion para comprobar los 4 casos, cada uno con una llamada
                         // Le pasamos el movimientosPosibles y que lo devuelva modificado, a no ser que 
                         // consigamos pasarle un puntero
@@ -272,30 +235,32 @@ public class tablero_Script : MonoBehaviour
                         movimientosPosibles = evaluarPosicion(tablero, i - 1, j, movimientosPosibles);
                         movimientosPosibles = evaluarPosicion(tablero, i, j + 1, movimientosPosibles);
                         movimientosPosibles = evaluarPosicion(tablero, i, j - 1, movimientosPosibles);
-
                     }
                 }
             }
             //hacemos una lista de posiciones libres y de posiciones ocupadas
-            Coordenadas[] arrLibres=posicionesCoincidentes(1, movimientosPosibles);
+            Coordenadas[] arrLibres = posicionesCoincidentes(1, movimientosPosibles);
             Coordenadas[] arrOcupadas = posicionesCoincidentes(2, movimientosPosibles);
             // Falta elegir una random
-            if (arrLibres == null)//solo queda combatir
-            {
-                //TODO seleccionar posicion de arrOcupadas a random
-                //TODO realizar combate
-            }
-            else if (arrOcupadas == null)//solo queda expandirse
-            {
+            if (arrLibres != null) {
+                // Se puede hacer un movimiento a un espacio libre
+                Debug.Log("Hay una opcion de desplazamiento. La long del arrLibres es " + arrLibres.Length);
                 //seleccionar posicion a random
                 exito = true;
-                Coordenadas posExpansion=arrLibres[Random.Range(0, arrLibres.Length)];
-                tablero[posExpansion.getI(), posExpansion.getJ()]=this.color;
-                
+                Coordenadas posExpansion = arrLibres[Random.Range(0, arrLibres.Length)];
+                tablero[posExpansion.getI(), posExpansion.getJ()] = this.color;
+
+            }
+            else {
+                // Hay que luchar si o si
+
+                //TODO seleccionar posicion de arrOcupadas a random
+                //TODO realizar combate
+
+
             }
 
-            if (exito)
-            {
+            if (exito) {
                 tamaño++;
             }
 
@@ -304,66 +269,51 @@ public class tablero_Script : MonoBehaviour
 
         }
         //evalua si la posicion pasada es posible para la expansion y que tipo de expansion seria
-        private int[,] evaluarPosicion(int[,] tablero, int iAEvaluar, int jAEvaluar, int[,] movimientosPosibles)
-        {
-            if (TAMAÑO > iAEvaluar && iAEvaluar >= 0 && TAMAÑO > jAEvaluar && jAEvaluar >= 0 && tablero[iAEvaluar, jAEvaluar] != this.color)
-            {
-                if (tablero[iAEvaluar, jAEvaluar] == 0)
-                {
+        private int[,] evaluarPosicion(int[,] tablero, int iAEvaluar, int jAEvaluar, int[,] movimientosPosibles) {
+            if (TAMAÑO > iAEvaluar && iAEvaluar >= 0 && TAMAÑO > jAEvaluar && jAEvaluar >= 0 && tablero[iAEvaluar, jAEvaluar] != this.color) {
+                if (tablero[iAEvaluar, jAEvaluar] == 0) {
                     movimientosPosibles[iAEvaluar, jAEvaluar] = 1;
                 }
-                else
-                {
+                else {
                     movimientosPosibles[iAEvaluar, jAEvaluar] = 2;
                 }
             }
             return movimientosPosibles;
         }
-        private Coordenadas[] posicionesCoincidentes(int val, int[,] movimientosPosibles)
-        {
-            Coordenadas[] resultado = null;
+        private Coordenadas[] posicionesCoincidentes(int val, int[,] movimientosPosibles) {
+            Coordenadas[] resultado = new Coordenadas[] { };
             Coordenadas aux;
-            for(int i=0;i< TAMAÑO; i++)
-            {
-                for(int j=0;j< TAMAÑO; j++)
-                {
-                    if (movimientosPosibles[i, j] == val)
-                    {
+            for (int i = 0; i < TAMAÑO; i++) {
+                for (int j = 0; j < TAMAÑO; j++) {
+                    if (movimientosPosibles[i, j] == val) {
                         aux = new Coordenadas(i, j);
-                        aux.concat(resultado);
+                        resultado = aux.concat(resultado);
                     }
                 }
             }
             return resultado;
         }
-        public int getCiudadanos()
-        {
+        public int getCiudadanos() {
             int ciudadanos = 0;
 
-            foreach (int valor in stats)
-            {
-                if (valor == CIUDADANOS)
-                {
+            foreach (int valor in stats) {
+                if (valor == CIUDADANOS) {
                     ciudadanos++;
                 }
             }
             return ciudadanos;
         }
-        public int getMilitar()
-        {
+        public int getMilitar() {
             int militar = 0;
 
-            foreach (int valor in stats)
-            {
-                if (valor == MILITAR)
-                {
+            foreach (int valor in stats) {
+                if (valor == MILITAR) {
                     militar++;
                 }
             }
             return militar;
         }
-        public void setColor(int color)
-        {
+        public void setColor(int color) {
             this.color = color;
         }
     }
@@ -373,8 +323,7 @@ public class tablero_Script : MonoBehaviour
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// 
 
 
-    public class Combate
-    {
+    public class Combate {
 
         Ciudad ciudad1;
         Ciudad ciudad2;
@@ -382,23 +331,21 @@ public class tablero_Script : MonoBehaviour
         Ciudad ciudad4;
         int[,] tablero = new int[TAMAÑO, TAMAÑO];
 
-        public Combate(Ciudad ciudad1, Ciudad ciudad2, Ciudad ciudad3, Ciudad ciudad4)
-        {
+        public Combate(Ciudad ciudad1, Ciudad ciudad2, Ciudad ciudad3, Ciudad ciudad4) {
             this.ciudad1 = ciudad1;
             prepararCiudad(ciudad1, AZUL);
             this.ciudad2 = ciudad2;
             prepararCiudad(ciudad2, ROJO);
             this.ciudad3 = ciudad3;
-            prepararCiudad(ciudad3,VERDE);
+            prepararCiudad(ciudad3, VERDE);
             this.ciudad4 = ciudad4;
-            prepararCiudad(ciudad4,MORADO);
+            prepararCiudad(ciudad4, MORADO);
             inicializarTablero();
-           
+
         }
-        public void ejecutarCombate()
-        {
+        public void ejecutarCombate() {
             Debug.Log("2");
-            ciudad1=ejecutarTurno(ciudad1);
+            ciudad1 = ejecutarTurno(ciudad1);
             Debug.Log("3");
             ciudad2 = ejecutarTurno(ciudad2);
             Debug.Log("4");
@@ -409,44 +356,37 @@ public class tablero_Script : MonoBehaviour
 
 
         }
-        private Ciudad ejecutarTurno(Ciudad ciudad)
-        {
+        private Ciudad ejecutarTurno(Ciudad ciudad) {
             //comprobar si la ciudad sigue activa
-            if (ciudad.getTamaño() != 0)
-            {
+            if (ciudad.getTamaño() != 0) {
                 //incrementa valores de la ciudad
+                int poblacionAntigua = ciudad.getPoblacion();
                 ciudad.setPoblacion(ciudad.getPoblacion() + 1 + ciudad.getCiudadanos());
                 ciudad.setEjercito(ciudad.getEjercito() + 1 + ciudad.getMilitar());
                 //comprueba expansion
-                
-                //while ((((ciudad.getPoblacion() / 5) + 1) - ciudad.getTamaño()) != 0)
-                //{
-                    tableroYBool aux=ciudad.expandir(this.tablero);
-                    if (aux.getExito())
-                    {
+                int num_veces = ((poblacionAntigua % 5) + ciudad.getPoblacion() - poblacionAntigua) / 5;
+                for (int i = 0; i < num_veces; i++) {
+                    Debug.Log("Se ha llamado a ciudad.expandir, el numero de poblacion ha aumentado " + num_veces + " veces");
+                    tableroYBool aux = ciudad.expandir(this.tablero);
+                    if (aux.getExito()) {
                         ciudad.setTamaño(ciudad.getTamaño() + 1);
                     }
                     tablero = aux.getTablero();
-                //}
-                
-                
+                }
+
             }
             return ciudad;
 
 
         }
-        public int[,] getTablero()
-        {
+        public int[,] getTablero() {
             return tablero;
         }
-        public void inicializarTablero()
-        {
+        public void inicializarTablero() {
 
 
-            for (int i = 0; i < TAMAÑO; i++)
-            {
-                for (int j = 0; j < TAMAÑO; j++)
-                {
+            for (int i = 0; i < TAMAÑO; i++) {
+                for (int j = 0; j < TAMAÑO; j++) {
                     tablero[i, j] = 0;
                 }
             }
@@ -456,8 +396,7 @@ public class tablero_Script : MonoBehaviour
             tablero[0, TAMAÑO - 1] = VERDE;
             tablero[TAMAÑO - 1, TAMAÑO - 1] = MORADO;
         }
-        public void prepararCiudad(Ciudad ciudad, int color)
-        {
+        public void prepararCiudad(Ciudad ciudad, int color) {
             ciudad.setEjercito(0);
             ciudad.setPoblacion(1);
             ciudad.setTamaño(1);
@@ -468,61 +407,49 @@ public class tablero_Script : MonoBehaviour
 
     //////////////////////////////////////////////////////////////////////////
 
-    public class Coordenadas
-    {
+    public class Coordenadas {
         int i;
         int j;
-        public Coordenadas(int i, int j)
-        {
+        public Coordenadas(int i, int j) {
             this.i = i;
             this.j = j;
         }
-        public int getI()
-        {
+        public int getI() {
             return i;
         }
-        public int getJ()
-        {
+        public int getJ() {
             return j;
         }
-        public Coordenadas[] concat(Coordenadas[] aux)
-        {
-            if (aux != null)
-            {
+        public Coordenadas[] concat(Coordenadas[] aux) {
+            if (aux != null) {
                 Coordenadas[] devolver = new Coordenadas[aux.Length + 1];
-                for (int i = 0; i < aux.Length; i++)
-                {
+                for (int i = 0; i < aux.Length; i++) {
                     devolver[i] = aux[i];
                 }
                 devolver[devolver.Length - 1] = this;
                 return devolver;
             }
-            else
-            {
+            else {
                 Coordenadas[] devolver = { new Coordenadas(i, j) };
                 return devolver;
             }
-            
+
         }
     }
 
     /////////////////////////////////////////////
-    
-    public class tableroYBool
-    {
+
+    public class tableroYBool {
         int[,] tablero;
         bool exito;
-        public tableroYBool(int[,]tablero, bool exito)
-        {
+        public tableroYBool(int[,] tablero, bool exito) {
             this.tablero = tablero;
             this.exito = exito;
         }
-        public int[,] getTablero()
-        {
+        public int[,] getTablero() {
             return tablero;
         }
-        public bool getExito()
-        {
+        public bool getExito() {
             return exito;
         }
     }
